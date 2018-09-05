1b694c45296460b7601c3ad0f42e5201f15bc387
ylwhlhp <ylwhlhp@126.com>
2018-09-05 10:30:51 +0800

    热钱扫码交易验证

--- composer.js
+++ composer.js
@@ -186,6 +186,7 @@ async function writeTran(params, handleResult) {
 				//更新列表
 				light.refreshTranList(obj);
 				//返回到界面
+				alert("交易完成个");
 				handleResult('', obj.id);
 			}
 			catch (e) {
