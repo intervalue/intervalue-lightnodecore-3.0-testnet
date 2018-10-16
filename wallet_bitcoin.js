7cd4fd1b959f18df2cfe9194c5c50db27b396881
zhang0135789 <zz634682577>
2018-10-16 13:37:27 +0800

    delete
    update  secrethelper.js wallet_bitcoin.js
    
    insert
    
    is stable   n
    
    优化
    
    by zz

--- wallet_bitcoin.js
+++ wallet_bitcoin.js
@@ -12,23 +12,35 @@ let version = new Buffer([0x00]);
  * m / purpose' / coin_type' / account' / change / address_index
  */
 function getBitAddress(xprikey,account,change,address_index) {
-    let crypto = require('crypto');
-    let base58 = require('base-58');
-    let Bitcore = require('bitcore-lib');
-    var hdPriKey = Bitcore.HDPrivateKey.fromString(xprikey);
     var path = "m/44'/0'/"+ account +"'/"+ change +"/"+ address_index;
-    var publicKey = hdPriKey.derive(path);
-    var pubkey = publicKey.toBuffer();
 
-    var s = crypto.createHash("sha256").update(pubkey).digest();
-    var payload = crypto.createHash("ripemd160").update(s).digest();
+    let secre = require('./secrethelper');
+    let Bitcore = require('bitcore-lib');
+
+    var path1 = "m/44'/0'/"+ account + "'";
+    var path2 = "m/"+change + "/" + address_index;
+
+    var hdPriKey = Bitcore.HDPrivateKey.fromString(xprikey);
+
+    var privateKey = hdPriKey.derive(path1);
+
+    var xpubkey = Bitcore.HDPrivateKey(privateKey).xpubkey;
+    var hdPubkey = new Bitcore.HDPublicKey(xpubkey);
+    var pubkey = hdPubkey.derive(path2).publicKey.toBuffer();
+
+
+    // var pubkey222 = publicKey.publicKey.toBuffer().toString("base64");
+    // var pubkey = publicKey.toBuffer();
+
+    var s = secre.sha256hash(pubkey);
+    var payload = secre.ripemd160hash(s);
 
     var vp = Buffer.concat([version,payload] ,version.length + payload.length);
 
-    var checksum = crypto.createHash("sha256").update(crypto.createHash("sha256").update(vp).digest()).digest();
+    var checksum = secre.sha256hash(secre.sha256hash(vp));
     var vpc = Buffer.concat([vp,checksum.slice(0,4)] , vp.length+4);
 
-    var address =  base58.encode(vpc);;
+    var address =  secre.base58encode(vpc);
     return address;
 };
 
