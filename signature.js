16d4e5d5e46bb4abbfc595f154cde2d383c32dc5
zhang0135789 <zz634682577>
2018-11-09 14:48:44 +0800

    签名算法优化,

--- signature.js
+++ signature.js
@@ -10,7 +10,7 @@ var ecdsa = require('secp256k1');
  */
 exports.sign = function(hash, priv_key){
 	var res = ecdsa.sign(hash, priv_key);
-	return res.signature.toString("base64");
+	return 32 + res.signature.toString("base64");
 };
 
 /**
@@ -22,7 +22,7 @@ exports.sign = function(hash, priv_key){
  */
 exports.verify = function(hash, b64_sig, b64_pub_key){
 	try{
-		var signature = new Buffer(b64_sig, "base64"); // 64 bytes (32+32)
+		var signature = new Buffer(b64_sig.substring(2), "base64"); // 64 bytes (32+32)
 		return ecdsa.verify(hash, signature, new Buffer(b64_pub_key, "base64"));
 	}
 	catch(e){
