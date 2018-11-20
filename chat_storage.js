a0dc95d410806f025b93d9c3f49d9dd63b80e05b
ylwhlhp <ylwhlhp@126.com>
2018-11-20 17:17:19 +0800

    优化，聊天信息、交易信息刷新判断

--- chat_storage.js
+++ chat_storage.js
@@ -6,7 +6,7 @@ function store(correspondent_address, message, is_incoming, type) {
     var type = type || 'text';
     let msg = message.substring(-1,44);
     db.query("SELECT id FROM chat_messages WHERE message like ?",['%'+msg+'%'],function (rows) {
-        if(rows != undefined && rows.length == 1){
+        if (rows != undefined && rows.length == 1 && msg.length == 44 && type == 'transaction') {
             db.query("UPDATE chat_messages set message =? WHERE id = ?",[message,rows[0].id]);
         }else {
             db.query("INSERT INTO chat_messages ('correspondent_address', 'message', 'is_incoming', 'type') VALUES (?, ?, ?, ?)", [correspondent_address, message, is_incoming, type]);
